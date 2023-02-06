cbadd95e47edbd97f89f2cb42435cee4f5baf003
Ilia Sevostianov <i.sevostianov@innopolis.university>
2023-02-06 12:38:05 +0300

    update

--- Module12_Algorithms_Structures/isKPeriodic/main.cpp
+++ Module12_Algorithms_Structures/isKPeriodic/main.cpp
@@ -2,19 +2,20 @@
 #include <string>
 using namespace std;
 
-bool IsKPeriodic(string str, int k) {
+bool IsKPeriodic(const string& str, const int  k) {
     // One character strings are trivially periodic.
+    if (k == 0) return false;
+
     if (str.length() == 1) return true;
     
     // Check if the string is divisible by k
     if (str.length() % k) return false;
     
     // Check if the string consists of k equal substrings
-    string subStr = str.substr(0, k);
-    int len = k;
+    size_t len = k;
     while (len < str.length()) {
         // If the substring does not match, the string is not periodic
-        if (subStr != str.substr(len, k)) {
+        if (str.substr(0, k) != str.substr(len, k)) {
             return false;
         }
         len += k;
@@ -25,6 +26,7 @@ bool IsKPeriodic(string str, int k) {
 
 int main() 
 {   
-    cout << IsKPeriodic("aaaa", 1) << endl;
+    std::string str = "abab";
+    cout << IsKPeriodic(str, 2) << endl;
     return 0;
 }
