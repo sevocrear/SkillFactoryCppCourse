1404762c63525d67d4423e389438ee73d7a01a3e
Ilia Sevostianov <ilia.sevostianov@atom.team>
2023-10-30 10:33:19 +0300

    add args example

--- Template/main.cpp
+++ Template/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-using namespace std;
+#include <string>
+#include <thread>
 
-int main() {
-    char greeting[6] = {'H', 'e', 'l', 'l', 'o'};
-    cout << greeting; // Выведется Hello
+void print_sum(int p1, int p2, int p3)
+{
+    std::cout << "Sum = " << p1 + p2 + p3 << std::endl;
+}
+
+int main()
+{
+    int a = 3, b = 4, c = 5;
+    std::thread t(print_sum, a, b, c);
+    t.join();
     return 0;
 }
